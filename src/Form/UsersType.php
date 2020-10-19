236c31805de3d7ec62875d8d83111da1172336e5
citizenz7 <citizenz7@protonmail.com>
2020-10-19 22:15:30 +0200

    bottom bar and top bar fixes, CGU, A propos

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -6,17 +6,25 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UsersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'label' => 'Votre pseudo'
+            ])
             //->add('roles')
             //->add('password')
-            ->add('email')
-            ->add('avatar')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse e-mail'
+            ])
+            ->add('avatar', TextType::class, [
+                'label' => 'Avatar (URL)'
+            ])
         ;
     }
 
