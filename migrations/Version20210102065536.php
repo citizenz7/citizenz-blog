34e3868bd1ee65d558a1412479c0fa65e4b387ea
citizenz7 <citizenz7@protonmail.com>
2021-01-02 09:02:56 +0100

    fixes categories

--- migrations/Version20210102065536.php
+++ migrations/Version20210102065536.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201030190007 extends AbstractMigration
+final class Version20210102065536 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,11 +21,15 @@ final class Version20201030190007 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE articles ADD is_inactive TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE users CHANGE username username VARCHAR(20) NOT NULL, CHANGE email email VARCHAR(55) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE articles DROP is_inactive');
+        $this->addSql('DROP INDEX UNIQ_1483A5E9E7927C74 ON users');
+        $this->addSql('ALTER TABLE users CHANGE username username VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
